5dfc57388ee48f455fe5cc71abd86c28ea320d31
Osbaldo <osbaldo.are2000@gmail.com>
2025-02-21 14:43:52 -0600

    Actualice las letras y sincronice el style.css

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function () {
   const colors = ["green", "blue", "red"];
 
@@ -15,22 +14,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-=======
-document.addEventListener("DOMContentLoaded", () => {
-    const colores = ["green", "blue", "red"];
-    
-    function colorAleatorio(actual) {
-        let nuevoColor;
-        do {
-            nuevoColor = colores[Math.floor(Math.random() * colores.length)];
-        } while (nuevoColor === actual);
-        return nuevoColor;
-    }
-
-    document.body.addEventListener("click", (event) => {
-        if (event.target.tagName === "H5") {
-            event.target.style.color = colorAleatorio(event.target.style.color);
-        }
-    });
-});
->>>>>>> ac76640a49455cbec42b4d40265fe9e03791216f
